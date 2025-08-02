1998ed226e3bab36f417ff4449cb7d1da1246f9b
beroyy <beroy@dayback.sh>
2025-08-02 17:12:08 -0400

    nuke web

--- apps/web/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { config as nextConfig } from "@ticketsbot/eslint-config/next.js";
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-  ...nextConfig,
-  {
-    ignores: [".next/**", "out/**"],
-  },
-];
--- apps/web/stores/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useInitialSetupComplete = create<boolean>()(() => false);
-
-export type SetupState = "selecting" | "configuring" | "complete" | "ready";
-export const useSetupState = create<SetupState>()(() => "selecting");
